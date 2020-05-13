4d00cee9888a43ac21fc2d9d2771eace57815131
Hope.Phenom <chaoshengze@wanji.net.cn>
2020-05-13 13:59:30 +0800

    1.比较两个目录功能完成

--- FilesQuickCompareTool/MainWindow.xaml.cs
+++ FilesQuickCompareTool/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using MultithreadingScaffold;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -16,6 +17,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MessageBox = System.Windows.MessageBox;
+using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
 using SaveFileDialog = Microsoft.Win32.SaveFileDialog;
 
 namespace FilesQuickCompareTool
@@ -128,9 +130,33 @@ namespace FilesQuickCompareTool
             return dic;
         }
 
-        private void CompareFunction()
+        /// <summary>
+        /// 比较两个数据源，并返回一个体现比对结果的DataTable
+        /// </summary>
+        /// <param name="source_1st">1号源</param>
+        /// <param name="source_2nd">2号源</param>
+        /// <param name="from_1st">1号源来源</param>
+        /// <param name="from_2nd">2号源来源</param>
+        /// <returns></returns>
+        private DataTable CompareTwoSource(Dictionary<string, string> source_1st, Dictionary<string, string> source_2nd, string from_1st, string from_2nd)
         {
+            var dt = new DataTable();
+            dt.Columns.Add("结论");
+            dt.Columns.Add("文件名");
+            dt.Columns.Add("来源");
+            dt.Columns.Add("MD5值");
 
+            foreach (var kv in source_1st)
+                if (!source_2nd.ContainsKey(kv.Key))
+                    dt.Rows.Add("特有", kv.Key, from_1st, kv.Value);
+                else if (source_2nd[kv.Key] != source_1st[kv.Key])
+                    dt.Rows.Add("不同", kv.Key, "两者", kv.Value);
+
+            foreach (var kv in source_2nd)
+                if (!source_1st.ContainsKey(kv.Key))
+                    dt.Rows.Add("特有", kv.Key, from_2nd, kv.Value);
+
+            return dt;
         }
 
         #endregion
@@ -187,6 +213,66 @@ namespace FilesQuickCompareTool
             catch (Exception)
             {
 
+            }
+        }
+
+        /// <summary>
+        /// 比价两个目录
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Btn_CompareTwoFolder_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (Tbx_FolderPath.Text == "" || Tbx_FolderPath_2nd.Text == "")
+                    return;
+
+                var currTime = DateTime.Now;
+
+                FileList.Clear();
+                GetFolderFileList(Tbx_FolderPath.Text);
+                var dict1 = CalcFileList(Tbx_FolderPath.Text);
+
+                FileList.Clear();
+                GetFolderFileList(Tbx_FolderPath_2nd.Text);
+                var dict2 = CalcFileList(Tbx_FolderPath_2nd.Text);
+
+                var dt = CompareTwoSource(dict1, dict2, Tbx_FolderPath.Text, Tbx_FolderPath_2nd.Text);
+
+                var endTime = DateTime.Now;
+
+                dataGrid_Main.ItemsSource = dt.DefaultView;
+                MessageBox.Show($@"目录比对完成，共计比对{dict1.Count + dict2.Count}个文件，耗时{Math.Round((endTime - currTime).TotalSeconds)}秒！");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($@"比较目录异常，{ex.Message}");
+            }
+        }
+
+        /// <summary>
+        /// 从清单比较
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Btn_CompareFromList_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var ofd = new OpenFileDialog();
+                ofd.Title = "请选择一个清单文件...";
+                ofd.Filter = "清单文件(.txt)|*.txt";
+                ofd.ShowDialog();
+
+                if (ofd.FileName == null || ofd.FileName == "" || Tbx_FolderPath.Text == "")
+                    return;
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($@"从清单比较异常，{ex.Message}");
             }
         }
 
