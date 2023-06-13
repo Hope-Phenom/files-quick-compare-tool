59d56580811c78cd649b422f1fb7fa7b030e968a
Hope.Phenom <hope.phenom@foxmail.com>
2023-06-13 15:53:22 +0800

    [feat] 1. 升级多线程脚手架 2. 升级 .net 运行时，计算步骤改为异步处理 3. 添加日志区，将先前的弹窗提示信息改为日志区输出，提升使用体验

--- FilesQuickCompareTool/MainWindow.xaml.cs
+++ FilesQuickCompareTool/MainWindow.xaml.cs
@@ -1,22 +1,14 @@
-﻿using Microsoft.Win32;
-using MultithreadingScaffold;
+﻿using MultithreadingScaffold;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.IO;
-using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Forms;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using MessageBox = System.Windows.MessageBox;
 using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
 using SaveFileDialog = Microsoft.Win32.SaveFileDialog;
 
@@ -79,7 +71,7 @@ namespace FilesQuickCompareTool
             }
             catch (Exception ex)
             {
-                MessageBox.Show("GetMD5HashFromFile() fail,error:" + ex.Message);
+                PrintLog("GetMD5HashFromFile() fail, error: " + ex.Message);
                 return "";
             }
         }
@@ -110,13 +102,18 @@ namespace FilesQuickCompareTool
         /// </summary>
         /// <param name="basePath">基础路径，列表中的路径会删除此路径部分以获得相对路径</param>
         /// <returns></returns>
-        private Dictionary<string, string> CalcFileList(string basePath)
+        private Dictionary<string, string> CalcFileList(string basePath, Action finalAction)
         {
             var dic = new Dictionary<string, string>();
 
             var mTScaffold = new MTScaffold();
             mTScaffold.Workload = FileList.Count;
             mTScaffold.ThreadLimit = Environment.ProcessorCount * MuiltThreadGain;
+            mTScaffold.InNewThread = true;
+            mTScaffold.Final =() =>
+            {
+                finalAction?.Invoke();
+            };
             mTScaffold.Worker = (i) =>
             {
                 var file = FileList[i];
@@ -159,6 +156,20 @@ namespace FilesQuickCompareTool
             return dt;
         }
 
+        /// <summary>
+        /// 在日志区写入日志信息
+        /// </summary>
+        /// <param name="log">日志内容</param>
+        private void PrintLog(string log)
+        {
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                var info = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} | {log}";
+                Tbx_Info.Text += info + Environment.NewLine;
+                Tbx_Info.ScrollToEnd();
+            }));
+        }
+
         #endregion
 
         #region UI组件事件
@@ -168,35 +179,47 @@ namespace FilesQuickCompareTool
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void Btn_MakeCompareList_Click(object sender, RoutedEventArgs e)
+        private async void Btn_MakeCompareList_Click(object sender, RoutedEventArgs e)
         {
-            try
+            ChangeButtons(false);
+            PrintLog("开始生成比较列表清单……");
+
+            await Task.Run(() =>
             {
-                FileList.Clear();
-                var currTime = DateTime.Now;
+                Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    try
+                    {
+                        FileList.Clear();
+                        var currTime = DateTime.Now;
 
-                var sfd = new SaveFileDialog();
-                sfd.Filter = "文本文件(.txt)|*.txt";
-                sfd.ShowDialog();
+                        var sfd = new SaveFileDialog();
+                        sfd.Filter = "文本文件(.txt)|*.txt";
+                        sfd.ShowDialog();
 
-                if (sfd.FileName == null || sfd.FileName == "")
-                    return;
+                        if (sfd.FileName == null || sfd.FileName == "")
+                            return;
 
-                GetFolderFileList(Tbx_FolderPath.Text);
-                var dict = CalcFileList(Tbx_FolderPath.Text);
+                        GetFolderFileList(Tbx_FolderPath.Text);
+                        var dict = new Dictionary<string, string>();
+                        dict = CalcFileList(Tbx_FolderPath.Text, new Action(() =>
+                        {
+                            var ls = new List<string>();
+                            foreach (var keyValue in dict)
+                                ls.Add($@"{keyValue.Key},{keyValue.Value}");
 
-                var ls = new List<string>();
-                foreach (var keyValue in dict)
-                    ls.Add($@"{keyValue.Key},{keyValue.Value}");
+                            File.WriteAllLines(sfd.FileName, ls.ToArray());
 
-                File.WriteAllLines(sfd.FileName, ls.ToArray());
-
-                MessageBox.Show($@"列表生成完毕，共计计算{ls.Count}个文件，耗时{Math.Round((DateTime.Now - currTime).TotalSeconds)}秒！");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($@"生成比较清单异常，{ex.Message}");
-            }
+                            PrintLog($@"列表生成完毕，共计计算{ls.Count}个文件，耗时{Math.Round((DateTime.Now - currTime).TotalSeconds)}秒！");
+                            ChangeButtons(true);
+                        }));
+                    }
+                    catch (Exception ex)
+                    {
+                        PrintLog($@"生成比较清单异常，{ex.Message}");
+                    }
+                }));
+            });
         }
 
         /// <summary>
@@ -217,38 +240,55 @@ namespace FilesQuickCompareTool
         }
 
         /// <summary>
-        /// 比价两个目录
+        /// 比较两个目录
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void Btn_CompareTwoFolder_Click(object sender, RoutedEventArgs e)
+        private async void Btn_CompareTwoFolder_Click(object sender, RoutedEventArgs e)
         {
-            try
+            ChangeButtons(false);
+            PrintLog("开始比较两个目录……");
+
+            await Dispatcher.BeginInvoke(new Action(() =>
             {
-                if (Tbx_FolderPath.Text == "" || Tbx_FolderPath_2nd.Text == "")
-                    return;
+                try
+                {
+                    if (Tbx_FolderPath.Text == "" || Tbx_FolderPath_2nd.Text == "")
+                        return;
 
-                var currTime = DateTime.Now;
+                    var currTime = DateTime.Now;
 
-                FileList.Clear();
-                GetFolderFileList(Tbx_FolderPath.Text);
-                var dict1 = CalcFileList(Tbx_FolderPath.Text);
+                    FileList.Clear();
+                    GetFolderFileList(Tbx_FolderPath.Text);
+                    var dict1 = new Dictionary<string, string>();
+                    dict1 = CalcFileList(Tbx_FolderPath.Text, new Action(() =>
+                    {
+                        Dispatcher.BeginInvoke((Action)(() =>
+                        {
+                            FileList.Clear();
+                            GetFolderFileList(Tbx_FolderPath_2nd.Text);
+                            var dict2 = new Dictionary<string, string>();
+                            dict2 = CalcFileList(Tbx_FolderPath_2nd.Text, new Action(() =>
+                            {
+                                Dispatcher.BeginInvoke(new Action(() =>
+                                {
+                                    var dt = CompareTwoSource(dict1, dict2, Tbx_FolderPath.Text, Tbx_FolderPath_2nd.Text);
 
-                FileList.Clear();
-                GetFolderFileList(Tbx_FolderPath_2nd.Text);
-                var dict2 = CalcFileList(Tbx_FolderPath_2nd.Text);
+                                    var endTime = DateTime.Now;
 
-                var dt = CompareTwoSource(dict1, dict2, Tbx_FolderPath.Text, Tbx_FolderPath_2nd.Text);
-
-                var endTime = DateTime.Now;
-
-                dataGrid_Main.ItemsSource = dt.DefaultView;
-                MessageBox.Show($@"目录比对完成，共计比对{dict1.Count + dict2.Count}个文件，耗时{Math.Round((endTime - currTime).TotalSeconds)}秒！");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($@"比较目录异常，{ex.Message}");
-            }
+                                    dataGrid_Main.ItemsSource = dt.DefaultView;
+                                    PrintLog($@"目录比对完成，共计比对{dict1.Count + dict2.Count}个文件，耗时{Math.Round((endTime - currTime).TotalSeconds)}秒！");
+                                    ChangeButtons(true);
+                                }));
+                            }));
+                        }));
+                    }));
+                }
+                catch (Exception ex)
+                {
+                    PrintLog($@"比较目录异常，{ex.Message}");
+                }
+            }));
         }
 
         /// <summary>
@@ -256,43 +296,55 @@ namespace FilesQuickCompareTool
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void Btn_CompareFromList_Click(object sender, RoutedEventArgs e)
+        private async void Btn_CompareFromList_Click(object sender, RoutedEventArgs e)
         {
-            try
+            ChangeButtons(false);
+            PrintLog("开始从清单比较……");
+
+            await Dispatcher.BeginInvoke(new Action(() =>
             {
-                var ofd = new OpenFileDialog();
-                ofd.Title = "请选择一个清单文件...";
-                ofd.Filter = "清单文件|*.txt";
-                ofd.ShowDialog();
-
-                if (ofd.FileName == null || ofd.FileName == "" || Tbx_FolderPath.Text == "")
-                    return;
-
-                var currTime = DateTime.Now;
-
-                var arr = File.ReadAllLines(ofd.FileName);
-                var dict2 = new Dictionary<string, string>();
-                foreach (var line in arr)
+                try
                 {
-                    var _arr = line.Split(',');
-                    dict2.Add(_arr[0], _arr[1]);
+                    var ofd = new OpenFileDialog();
+                    ofd.Title = "请选择一个清单文件...";
+                    ofd.Filter = "清单文件|*.txt";
+                    ofd.ShowDialog();
+
+                    if (ofd.FileName == null || ofd.FileName == "" || Tbx_FolderPath.Text == "")
+                        return;
+
+                    var currTime = DateTime.Now;
+
+                    var arr = File.ReadAllLines(ofd.FileName);
+                    var dict2 = new Dictionary<string, string>();
+                    foreach (var line in arr)
+                    {
+                        var _arr = line.Split(',');
+                        dict2.Add(_arr[0], _arr[1]);
+                    }
+
+                    FileList.Clear();
+                    GetFolderFileList(Tbx_FolderPath.Text);
+                    var dict1 = new Dictionary<string, string>();
+                    CalcFileList(Tbx_FolderPath.Text, new Action(() =>
+                    {
+                        Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            var dt = CompareTwoSource(dict1, dict2, Tbx_FolderPath.Text, $@"清单文件:{ofd.FileName}");
+
+                            var endTime = DateTime.Now;
+
+                            dataGrid_Main.ItemsSource = dt.DefaultView;
+                            PrintLog($@"目录比对完成，共计比对{dict1.Count + dict2.Count}个文件，耗时{Math.Round((endTime - currTime).TotalSeconds)}秒！");
+                            ChangeButtons(true);
+                        }));
+                    }));
                 }
-
-                FileList.Clear();
-                GetFolderFileList(Tbx_FolderPath.Text);
-                var dict1 = CalcFileList(Tbx_FolderPath.Text);
-
-                var dt = CompareTwoSource(dict1, dict2, Tbx_FolderPath.Text, $@"清单文件:{ofd.FileName}");
-
-                var endTime = DateTime.Now;
-
-                dataGrid_Main.ItemsSource = dt.DefaultView;
-                MessageBox.Show($@"目录比对完成，共计比对{dict1.Count + dict2.Count}个文件，耗时{Math.Round((endTime - currTime).TotalSeconds)}秒！");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($@"从清单比较异常，{ex.Message}");
-            }
+                catch (Exception ex)
+                {
+                    PrintLog($@"从清单比较异常，{ex.Message}");
+                }
+            }));
         }
 
         /// <summary>
@@ -300,45 +352,70 @@ namespace FilesQuickCompareTool
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void Btn_CompareTwoList_Click(object sender, RoutedEventArgs e)
+        private async void Btn_CompareTwoList_Click(object sender, RoutedEventArgs e)
         {
-            var ofd = new OpenFileDialog();
-            ofd.Title = "请选择第一个清单文件...";
-            ofd.Filter = "清单文件|*.txt";
-            ofd.ShowDialog();
+            ChangeButtons(false);
+            PrintLog("开始从比较两个清单……");
 
-            var ofd_2nd = new OpenFileDialog();
-            ofd_2nd.Title = "请选择第二个清单文件...";
-            ofd_2nd.Filter = "清单文件|*.txt";
-            ofd_2nd.ShowDialog();
-
-            if (ofd.FileName == null || ofd.FileName == "" || ofd_2nd.FileName == null || ofd_2nd.FileName == "")
-                return;
-
-            var currTime = DateTime.Now;
-
-            var arr = File.ReadAllLines(ofd.FileName);
-            var dict1 = new Dictionary<string, string>();
-            foreach (var line in arr)
+            await Dispatcher.BeginInvoke(new Action(() =>
             {
-                var _arr = line.Split(',');
-                dict1.Add(_arr[0], _arr[1]);
-            }
+                Dispatcher.BeginInvoke((Action)(() =>
+                {
+                    var ofd = new OpenFileDialog();
+                    ofd.Title = "请选择第一个清单文件...";
+                    ofd.Filter = "清单文件|*.txt";
+                    ofd.ShowDialog();
 
-            var arr2 = File.ReadAllLines(ofd_2nd.FileName);
-            var dict2 = new Dictionary<string, string>();
-            foreach (var line in arr2)
+                    var ofd_2nd = new OpenFileDialog();
+                    ofd_2nd.Title = "请选择第二个清单文件...";
+                    ofd_2nd.Filter = "清单文件|*.txt";
+                    ofd_2nd.ShowDialog();
+
+                    if (ofd.FileName == null || ofd.FileName == "" || ofd_2nd.FileName == null || ofd_2nd.FileName == "")
+                        return;
+
+                    var currTime = DateTime.Now;
+
+                    var arr = File.ReadAllLines(ofd.FileName);
+                    var dict1 = new Dictionary<string, string>();
+                    foreach (var line in arr)
+                    {
+                        var _arr = line.Split(',');
+                        dict1.Add(_arr[0], _arr[1]);
+                    }
+
+                    var arr2 = File.ReadAllLines(ofd_2nd.FileName);
+                    var dict2 = new Dictionary<string, string>();
+                    foreach (var line in arr2)
+                    {
+                        var _arr = line.Split(',');
+                        dict2.Add(_arr[0], _arr[1]);
+                    }
+
+                    var dt = CompareTwoSource(dict1, dict2, $@"清单文件:{ofd.FileName}", $@"清单文件:{ofd_2nd.FileName}");
+
+                    var endTime = DateTime.Now;
+
+                    dataGrid_Main.ItemsSource = dt.DefaultView;
+                    PrintLog($@"目录比对完成，共计比对{dict1.Count + dict2.Count}个文件，耗时{Math.Round((endTime - currTime).TotalSeconds)}秒！");
+                    ChangeButtons(true);
+                }));
+            }));
+        }
+
+        /// <summary>
+        /// 改变按钮状态
+        /// </summary>
+        /// <param name="enable">可用性</param>
+        private void ChangeButtons(bool enable)
+        {
+            Dispatcher.BeginInvoke(new Action(() =>
             {
-                var _arr = line.Split(',');
-                dict2.Add(_arr[0], _arr[1]);
-            }
-
-            var dt = CompareTwoSource(dict1, dict2, $@"清单文件:{ofd.FileName}", $@"清单文件:{ofd_2nd.FileName}");
-
-            var endTime = DateTime.Now;
-
-            dataGrid_Main.ItemsSource = dt.DefaultView;
-            MessageBox.Show($@"目录比对完成，共计比对{dict1.Count + dict2.Count}个文件，耗时{Math.Round((endTime - currTime).TotalSeconds)}秒！");
+                Btn_CompareFromList.IsEnabled =
+                    Btn_CompareTwoFolder.IsEnabled =
+                    Btn_CompareTwoList.IsEnabled =
+                    Btn_MakeCompareList.IsEnabled = enable;
+            }));
         }
 
         #endregion
